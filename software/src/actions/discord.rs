46fdabe17049ed8349211df86f806ea51fc54b21
Logan H-D <notquiteapex@gmail.com>
2025-03-11 22:36:10 -0400

    basic discord integration working

--- software/src/actions/discord.rs
+++ software/src/actions/discord.rs
@@ -1,11 +1,19 @@
+use std::{collections::HashMap, error::Error, fmt, sync::OnceLock};
+
 use anyhow::Result;
+use discord_rich_presence_client::{voice_settings::VoiceSettings, DiscordIpc, DiscordIpcClient};
 use eframe::egui::{vec2, Button, Ui};
 use egui_phosphor::regular as phos;
 use serde::{Deserialize, Serialize};
+use tokio::{sync::Mutex, task::spawn_blocking};
 
 use crate::{config::JukeBoxConfig, input::InputKey};
 
 use super::types::{Action, ActionType as AT};
+
+const DISCORD_CLIENT_ID: Option<&str> = option_env!("JUKEBOXDESKTOP_DISCORD_CLIENT_ID");
+const DISCORD_CLIENT_SECRET: Option<&str> = option_env!("JUKEBOXDESKTOP_DISCORD_CLIENT_SECRET");
+static DISCORD_CLIENT: OnceLock<Mutex<DiscordIpcClient>> = OnceLock::new();
 
 #[rustfmt::skip]
 pub fn discord_action_list() -> (String, Vec<(AT, Box<dyn Action>, String)>) {
@@ -20,22 +28,89 @@ pub fn discord_action_list() -> (String, Vec<(AT, Box<dyn Action>, String)>) {
     )
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct OauthAccess {
+    token_type: String,
+    access_token: String,
+    expires_in: usize,
+    refresh_token: String,
+    scope: String,
+}
+#[derive(Debug)]
+struct OauthAccessError {}
+impl fmt::Display for OauthAccessError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "failed to gain oauth access")
+    }
+}
+impl Error for OauthAccessError {}
+
+fn discord_access_token_request(
+    code: &str,
+    client_id: &str,
+    client_secret: &str,
+) -> Result<OauthAccess, Box<dyn std::error::Error>> {
+    let client = reqwest::blocking::Client::new();
+    let params = HashMap::from([
+        ("grant_type", "authorization_code"),
+        ("code", code),
+        ("redirect_uri", "http://localhost:61961"),
+        ("client_id", client_id),
+        ("client_secret", client_secret),
+    ]);
+
+    let r = client
+        .post("https://discord.com/api/oauth2/token")
+        .form(&params)
+        .send()?;
+
+    if let Ok(j) = r.json() {
+        log::debug!("{:?}", j);
+        Ok(j)
+    } else {
+        Err(Box::new(OauthAccessError {}))
+    }
+}
+
 fn account_warning(ui: &mut Ui) {
-    ui.vertical_centered(|ui| ui.label(t!("action.discord.warning.help")));
-    ui.label("");
-    if ui
-        .add_sized(
-            vec2(228.0, 110.0),
-            Button::new(t!("action.discord.warning.button")),
-        )
-        .clicked()
-    {
-        log::info!("TODO: Discord integration");
+    if DISCORD_CLIENT.get().is_none() {
+        ui.vertical_centered(|ui| ui.label(t!("action.discord.warning.help")));
+        ui.label("");
+        if ui
+            .add_sized(
+                vec2(228.0, 110.0),
+                Button::new(t!("action.discord.warning.button")),
+            )
+            .clicked()
+        {
+            let mut client = DiscordIpcClient::new(DISCORD_CLIENT_ID.unwrap());
+            client.connect().expect("cannot connect to discord");
+            let code = client
+                .authorize(&["rpc", "rpc.voice.read", "rpc.voice.write"])
+                .expect("failed to authorize wtih discord");
+            let oauth = discord_access_token_request(
+                &code,
+                DISCORD_CLIENT_ID.unwrap(),
+                DISCORD_CLIENT_SECRET.unwrap(),
+            )
+            .expect("failed to get oauth access token");
+            client
+                .authenticate(&oauth.access_token)
+                .expect("failed to authenticate with discord");
+
+            DISCORD_CLIENT
+                .set(Mutex::new(client))
+                .expect("failed to set DISCORD_CLIENT");
+        }
+    } else {
+        ui.vertical_centered(|ui| ui.label(t!("action.discord.warning.success")));
     }
 }
 
 #[derive(Default, Serialize, Deserialize, Clone)]
-pub struct DiscordToggleMute {}
+pub struct DiscordToggleMute {
+    // muted: bool,
+}
 #[async_trait::async_trait]
 #[typetag::serde]
 impl Action for DiscordToggleMute {
@@ -46,6 +121,18 @@ impl Action for DiscordToggleMute {
         _config: &mut JukeBoxConfig,
     ) -> Result<()> {
         // TODO
+        let _ = spawn_blocking(|| {
+            if DISCORD_CLIENT.get().is_none() {
+                return;
+            }
+
+            let mut client = DISCORD_CLIENT.get().unwrap().blocking_lock();
+            client
+                .set_voice_settings(VoiceSettings::new().mute(true))
+                .expect("fuck");
+        })
+        .await;
+
         Ok(())
     }
 
