c82667da5b5e113f0141410710f2559723749355
Logan H-D <notquiteapex@gmail.com>
2025-04-06 14:43:34 -0400

    overhaul options for soundboard

--- desktop/src/actions/soundboard.rs
+++ desktop/src/actions/soundboard.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use eframe::egui::{include_image, ComboBox, ImageSource, Slider, Ui};
+use eframe::egui::{include_image, ComboBox, ImageSource, Slider, TextWrapMode, Ui};
 use egui_phosphor::regular as phos;
 use rfd::FileDialog;
 use serde::{Deserialize, Serialize};
@@ -15,6 +15,24 @@ pub const AID_SOUNDBOARD_PLAY_SOUND: &str = "SoundboardPlaySound";
 const ICON_PLAY_SOUND: ImageSource =
     include_image!("../../../assets/action-icons/soundboard-play.bmp");
 
+#[derive(Default, Serialize, Deserialize, Clone, Copy, PartialEq, Eq)]
+enum PlayMethod {
+    #[default]
+    PlayStop,
+    PlayOverlap,
+    PlayRestart,
+    LoopStop,
+}
+
+#[derive(Default, Serialize, Deserialize, Clone, Copy, PartialEq, Eq)]
+enum FadeMethod {
+    #[default]
+    NoFade,
+    FadeIn,
+    FadeOut,
+    FadeInAndOut,
+}
+
 #[rustfmt::skip]
 pub fn soundboard_action_list() -> (String, Vec<(String, Box<dyn Action>, String)>) {
     (
@@ -26,8 +44,11 @@ pub fn soundboard_action_list() -> (String, Vec<(String, Box<dyn Action>, String
 #[derive(Default, Serialize, Deserialize, Clone)]
 pub struct SoundboardPlaySound {
     filepath: String,
-    output_device: String,
     volume: u8,
+    play_method: PlayMethod,
+    fade_method: FadeMethod,
+    fade_time: u8,
+    output_device: String,
 }
 #[async_trait::async_trait]
 #[typetag::serde]
@@ -74,16 +95,94 @@ impl Action for SoundboardPlaySound {
 
         ui.label("");
 
-        ui.label(t!("action.soundboard.play_sound.output_device"));
-        ComboBox::from_id_salt("SoundboardPlaySoundDeviceSelect")
-            .selected_text(self.output_device.clone())
-            .width(228.0)
-            .show_ui(ui, |_ui| {});
+        ui.label(t!("action.soundboard.play_sound.volume"));
+        ui.add(Slider::new(&mut self.volume, 0..=100));
 
         ui.label("");
 
-        ui.label(t!("action.soundboard.play_sound.volume"));
-        ui.add(Slider::new(&mut self.volume, 0..=100));
+        let combo = &[
+            (
+                PlayMethod::PlayStop,
+                t!("action.soundboard.play_sound.play_method.play_stop"),
+            ),
+            (
+                PlayMethod::PlayOverlap,
+                t!("action.soundboard.play_sound.play_method.play_overlap"),
+            ),
+            (
+                PlayMethod::PlayRestart,
+                t!("action.soundboard.play_sound.play_method.play_restart"),
+            ),
+            (
+                PlayMethod::LoopStop,
+                t!("action.soundboard.play_sound.play_method.loop_stop"),
+            ),
+        ];
+
+        let current_combo = combo.iter().position(|c| c.0 == self.play_method).unwrap();
+
+        ui.label(t!("action.soundboard.play_sound.play_method.title"));
+        ComboBox::from_id_salt("SoundboardPlaySoundPlayMethod")
+            .selected_text(combo[current_combo].1.clone())
+            .width(200.0)
+            .wrap_mode(TextWrapMode::Truncate)
+            .show_ui(ui, |ui| {
+                for c in combo {
+                    ui.selectable_value(&mut self.play_method, c.0, c.1.clone());
+                }
+            });
+
+        ui.label("");
+
+        let combo = &[
+            (
+                FadeMethod::NoFade,
+                t!("action.soundboard.play_sound.fade_method.no_fade"),
+            ),
+            (
+                FadeMethod::FadeIn,
+                t!("action.soundboard.play_sound.fade_method.fade_in"),
+            ),
+            (
+                FadeMethod::FadeOut,
+                t!("action.soundboard.play_sound.fade_method.fade_out"),
+            ),
+            (
+                FadeMethod::FadeInAndOut,
+                t!("action.soundboard.play_sound.fade_method.fade_in_and_out"),
+            ),
+        ];
+
+        let current_combo = combo.iter().position(|c| c.0 == self.fade_method).unwrap();
+
+        ui.label(t!("action.soundboard.play_sound.fade_method.title"));
+        ComboBox::from_id_salt("SoundboardPlaySoundFadeMethod")
+            .selected_text(combo[current_combo].1.clone())
+            .width(200.0)
+            .wrap_mode(TextWrapMode::Truncate)
+            .show_ui(ui, |ui| {
+                for c in combo {
+                    ui.selectable_value(&mut self.fade_method, c.0, c.1.clone());
+                }
+            });
+
+        if self.fade_method != FadeMethod::NoFade {
+            ui.label("");
+
+            ui.label(t!("action.soundboard.play_sound.fade_time"));
+            ui.add(Slider::new(&mut self.fade_time, 0..=5));
+        }
+
+        ui.label("");
+
+        ui.label(t!("action.soundboard.play_sound.output_device"));
+        ComboBox::from_id_salt("SoundboardPlaySoundDeviceSelect")
+            .selected_text(self.output_device.clone())
+            .width(200.0)
+            .wrap_mode(TextWrapMode::Truncate)
+            .show_ui(ui, |_ui| {
+                // TODO
+            });
     }
 
     fn help(&self) -> String {
